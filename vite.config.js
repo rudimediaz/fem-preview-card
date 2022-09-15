6eade13a42923b5b5931e4ccfe0b31a560bffc1d
Rudhi Mediastara <rudhi.mediastara@gmail.com>
2022-09-15 19:59:06 +0700

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+
+const port = 3000;
+
+export default defineConfig({
+  server: {
+    port,
+    hmr: resolveHMR(),
+  },
+});
+
+function resolveHMR() {
+  if (process.env.GITPOD_WORKSPACE_URL) {
+    return {
+      // removes the protocol and replaces it with the port we're connecting to
+      host: process.env.GITPOD_WORKSPACE_URL.replace(
+        "https://",
+        `${port}-`
+      ),
+      protocol: "wss",
+      clientPort: 443,
+    };
+  }
+
+  if (process.env.CODESPACES) {
+    return {
+      host: `${process.env.CODESPACE_NAME}-${port}.githubpreview.dev`,
+      protocol: "wss",
+      clientPort: 443,
+    };
+  }
+
+  return true;
+}
